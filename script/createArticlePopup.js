dd74f0e2977ea4c641c9459cb64917b7bc8ef6b4
fatau2 <rekerism@gmail.com>
2019-09-09 22:59:42 +0700

    added css

---
+++ script/createArticlePopup.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.btn-invitation-to-write').click(function(){
+        $('.quill-container').css({ display : 'block' })
+    })
+
+    $('.btn-close-popup').click(function(){
+        $('.quill-container').css({ display : 'none'})
+    })
+ });
+
+ 

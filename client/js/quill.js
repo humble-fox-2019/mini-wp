fd15d7cc3e2ed3fe4b1b5aa19d2fa0765d5d3661
Tigor <tigor.hutasuhut@gmail.com>
2019-09-10 01:33:13 +0700

    merapikan halaman post

---
+++ client/js/quill.js
@@ -0,0 +1,34 @@
+hljs.configure({   // optionally configure hljs
+	languages: ['javascript', 'json', 'markdown']
+});
+
+var quill = new Quill('#editor', {
+	modules: {
+		toolbar: [
+
+			['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+			['blockquote', 'code-block'],
+
+			[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+			[{ 'list': 'ordered' }, { 'list': 'bullet' }],
+			[{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+			[{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+			[{ 'direction': 'rtl' }],                         // text direction
+
+			[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+			[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+			[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+			[{ 'font': [] }],
+			[{ 'align': [] }],
+
+			['clean']
+		],
+		history: {
+			delay: 2000,
+			maxStack: 500
+		},
+		modules: { syntax: true }
+	},
+	theme: 'snow'
+});

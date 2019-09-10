da99c79bfd41dac1f36997e514c0bf2502b3a022
igun369 <guntoro.gyk@gmail.com>
2019-09-10 13:44:42 +0700

    display articles and filter article by title

---
+++ client/js/index.js
@@ -0,0 +1,39 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        articles: [],
+        searchByTitle: '',
+        loading: false
+    },
+    methods: {
+        fetchArticles: function() {
+            axios({
+                url: 'http://localhost:3000/articles',
+                method: 'GET'
+            })
+            .then(response => {
+                this.articles = response.data;
+                // this.loading = true
+                // console.log(this.articles)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    },
+    computed: {
+        searchedArticles() {
+            let title = this.searchByTitle;
+            let filteredArticle = [];
+            this.articles.forEach(article => {
+                if (article.title.toLowerCase() === title.toLowerCase()) {
+                    filteredArticle.push(article)
+                }
+            });
+            return filteredArticle;
+        }
+    },
+    created : function () {
+        this.fetchArticles();
+    }
+})
--- server/app.js
+++ server/app.js
@@ -4,21 +4,19 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect('mongodb://localhost:27017/mini-wp', { useNewUrlParser: true })
+    .then(ok => {
+        console.log('mongodb connected!')
+    })
+    .catch(err => {
+        console.log('cannot connect to mongodb!', err);
+    })
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use('/', routes);
 
-mongoose.connect('mongodb://localhost:27017/mini-wp', { useNewUrlParser: true })
-    .then(ok => {
-        console.log('mongodb connected!')
-        return app.listen(PORT)
-    })
-    .then(ok => {
-        console.log('app listening on port', PORT);
-    })
-    .catch(err => {
-        console.log('cannot connect to mongodb!', err);
-    })
+app.listen(PORT, () => console.log('app listening on port', PORT));

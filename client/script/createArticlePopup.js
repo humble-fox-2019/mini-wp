eeb786a1b06f882708b421ad100ea0c6aeac4286
fatau2 <rekerism@gmail.com>
2019-09-11 14:38:10 +0700

    perfected crud to server

--- client/script/createArticlePopup.js
+++ client/script/createArticlePopup.js
@@ -1,10 +1,12 @@
+//Not used anymore
+
 $(document).ready(function(){
 
-    $('.btn-invitation-to-write, .btn-update-article').click(function(){
+    $('btn-invitation-to-write, .btn-update-article').click(function(){
         $('#article-form').css({ display : 'block' })
     })
 
-    $('.btn-close-popup').click(function(){
+    $('btn-close-popup').click(function(){
         $('#article-form').css({ display : 'none'})
     })
  });

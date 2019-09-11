93abdea59609be7aa96c25ba1ab2f5379e2765bd
Ayu Sudi <ayusudi.abc@gmail.com>
2019-09-11 17:42:03 +0700

    ayu

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const cors = require('cors')
 const mongoose = require('mongoose')
 const index = require('./routes/index')

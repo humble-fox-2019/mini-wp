29ddc1abcd93bdc7ad0dd4c62cfd14858458558f
YusufBaraja <baraja.yusuf@gmail.com>
2019-09-11 15:39:35 +0700

    deploy gcp

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const article = require('./article');
+const images = require('../middlewares/image')
 
 
-router.use('/article', article);
+ router.use('/article', article);
 
+// router.get('/', (req, res, next) => {
+//     res.send({ message: 'Welcome Buddy!' })
+//   })
+
+  router.post('/upload',
+    images.multer.single('image'), 
+    images.sendUploadToGCS,
+    (req, res) => {
+      res.send({
+        status: 200,
+        message: 'Your file is successfully uploaded',
+        link: req.file.cloudStoragePublicUrl
+      })
+    })
 
 
 module.exports = router;

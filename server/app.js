543bee2f240d5084f5a544eb180bd9e4ca8d0bc5
igun369 <guntoro.gyk@gmail.com>
2019-09-11 14:12:35 +0700

    add mongodb env

--- server/app.js
+++ server/app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV === 'development') {
+    console.log(process.env.NODE_ENV)
+    require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -5,9 +10,10 @@ const routes = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://localhost:27017/mini-wp-igun', { useNewUrlParser: true })
+mongoose.connect(`${MONGODB_URI}`, { useNewUrlParser: true })
     .then(ok => {
         console.log('mongodb connected!')
     })

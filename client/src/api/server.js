721ca6e981a83eeaca18f48eed4e7ab39df66345
igun369 <guntoro.gyk@gmail.com>
2019-09-16 04:19:27 +0700

    change server

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'http://api.mindlee.guntoroyk.site'
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://api.mindlee.guntoroyk.site'
+  // baseURL: 'http://localhost:3001'
 })
 
 export default instance;
--- client/src/components/Auth-GSignIn.vue
+++ client/src/components/Auth-GSignIn.vue
@@ -1,5 +1,6 @@
 <template>
   <button v-google-signin-button="clientId" class="google-signin-button"> Continue with Google</button>
+
 </template>
 
 <script>
@@ -48,6 +49,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+button {
+  
+}
 </style>

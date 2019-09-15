ddd0193aee0d4d362dfae9e16acf806b056ace03
TatagW <tatagwibowo95@gmail.com>
2019-09-16 03:53:31 +0700

    hadeh

--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-import GSignInButton from 'vue-google-signin-button'
-Vue.use(GSignInButton)
-new Vue({
-    render: createElement => createElement(App)
-}).$mount('#app');

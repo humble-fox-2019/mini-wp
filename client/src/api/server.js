cfbc66c111e8e0a4fbf96d965ae1a98c1bfa3687
guntoroyk <guntoro.gyk@gmail.com>
2020-05-28 17:15:00 +0700

    ready to deploy on heroku

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://api.mindlee.guntoroyk.site'
+  // baseURL: 'http://api.mindlee.guntoroyk.site'
   // baseURL: 'http://localhost:3001'
+  baseURL: 'https://mini-wp-server.herokuapp.com'
 })
 
 export default instance;
